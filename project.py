42c836f9d0938275f70e5ccc29592b267d63e6f3
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2019-10-03 19:19:42 -0600

    terminado

--- project.py
+++ project.py
@@ -4,97 +4,110 @@
 # Archivo: proyect.py
 # Fecha: 4/10/2019
 
+#IMPORTANTE: usted ingresa la matriz y le dice que propiedades tiene en conjunto. No evalua propiedad por propiedo
 
 # Para todas las operaciones de matrices
 # Se necesita de Numpy
 import numpy as np
 
+
 def validateNum(value):
-    #Valida si el ingreso del numero 
-    #Es un valor correcto 
+    # Valida si el ingreso del numero
+    # Es un valor correcto
     try:
         int(value)
         return True
     except ValueError:
         return False
-#Verifica que el rango sea un valor n=n debido a 
-#que las matrices que se están trabajando son cuadradas. 
+# Verifica que el rango sea un valor n=n debido a
+# que las matrices que se están trabajando son cuadradas.
+
+
 def isValueInRange(testValue, num1, num2):
-    #Verifica si el test value pertenecen 
-    #al rango establecido por num1 y num2
+    # Verifica si el test value pertenecen
+    # al rango establecido por num1 y num2
     if(testValue >= num1 and testValue <= num2) or (testValue <= num1 and testValue >= num2):
         return True
     return False
 
-# Se declaran las propiedades de 
-# una MATRIZ DE RELACIÓN 
+# Se declaran las propiedades de
+# una MATRIZ DE RELACIÓN
 ################################################F u n  t i o n s###########################################################################
 
-#Se verifica si es una matriz simétrica 
-#parametro: una matriz binaria 
-#retorno: devuelve True si A (intersección) AT (transposición) <= I
-def isSymmetric(binaryMatrix):
-    return(binaryMatrix.equalsOfMatrix(binaryMatrix.transposedOfMatrix())) #Se usa directo la libreria de np 
+# Se verifica si es una matriz simétrica
+# parametro: una matriz binaria
+# retorno: devuelve True si A (intersección) AT (transposición) <= I
 
-#Se verifica si es reflexiva 
-#parameto: una matriz binaria
-#retorno: devuelve *True* si A = A ** T (matriz transpuesta)
+
+def isSymmetric(binaryMatrix):
+    # Se usa directo la libreria de np
+    return(binaryMatrix.equalsOfMatrix(binaryMatrix.transposedOfMatrix()))
+
+# Se verifica si es reflexiva
+# parameto: una matriz binaria
+# retorno: devuelve *True* si A = A ** T (matriz transpuesta)
+
+
 def isReflexive(binaryMatrix):
-    #Se calcula la matriz identidad 
-    identMatrix = np.identity(binaryMatrix.matrix.shape[0], dtype=int) #uso de librería de np
-    #Se aplica la matriz identidad en la matriz binaria 
+    # Se calcula la matriz identidad
+    # uso de librería de np
+    identMatrix = np.identity(binaryMatrix.matrix.shape[0], dtype=int)
+    # Se aplica la matriz identidad en la matriz binaria
     identBinary = RelationMatrix(identMatrix)
-    #Retorno de resultado final
+    # Retorno de resultado final
     return(identBinary.precedenceOfMatrix(binaryMatrix.matrix))
 
-#Se verifica si es una matriz simétrica 
-#parametro: una matriz binaria 
-#retorno: devuelve True si A ** 2 <= A
+# Se verifica si es una matriz simétrica
+# parametro: una matriz binaria
+# retorno: devuelve True si A ** 2 <= A
+
+
 def isAntisymmetric(binaryMatrix):
-    #Se calcula la matriz identidad 
+    # Se calcula la matriz identidad
     identMatrix = np.identity(binaryMatrix.matrix.shape[0], dtype=int)
-    #Retorno de resultado final 
-    #Se aplica la intersección de la matriz transpuesta (composición)
+    # Retorno de resultado final
+    # Se aplica la intersección de la matriz transpuesta (composición)
     return((binaryMatrix.intersectionOfMatrix(binaryMatrix.transposedOfMatrix())).precedenceOfMatrix(identMatrix))
 
-#Verifica si una matriz es transitiva
-#parametro: una matriz binaria 
-#retorno: devuelve si es transitiva o no la matriz (su composición es igual)
+# Verifica si una matriz es transitiva
+# parametro: una matriz binaria
+# retorno: devuelve si es transitiva o no la matriz (su composición es igual)
+
+
 def isTransitive(binaryMatrix):
     return((binaryMatrix.multiplicationOfMatrix(binaryMatrix.matrix)).precedenceOfMatrix(binaryMatrix.matrix))
+
 
 class RelationMatrix(object):
 
     def __init__(self, matrix):
-        # Se declara una matriz de np 
+        # Se declara una matriz de np
         self.matrix = matrix
 
-    
     def transposedOfMatrix(self):
         return self.matrix.transpose()
 
     def equalsOfMatrix(self, firstMatrix):
         return np.array_equal(self.matrix, firstMatrix)
 
-
     def multiplicationOfMatrix(self, firstMatrix):
-        #Se realiza producto punto de matrices
+        # Se realiza producto punto de matrices
         dotProductOfMatrix = np.dot(self.matrix, firstMatrix)
         return (RelationMatrix(dotProductOfMatrix))
 
     def intersectionOfMatrix(self, firstMatrix):
-        #Se valida la interesccion entre matrices 
+        # Se valida la interesccion entre matrices
         valueA = np.array(self.matrix, dtype=bool)
         valueB = np.array(firstMatrix, dtype=bool)
         return RelationMatrix(1*np.logical_and(valueA, valueB))
 
     def precedenceOfMatrix(self, firstMatrix):
-        #Se evalua la precedencia de matrices / aun no terminado
+        # Se evalua la precedencia de matrices / aun no terminado
         boolMatrix = self.matrix <= firstMatrix
         boolMatrixTest = np.where(boolMatrix == False)[0]
         if(boolMatrixTest.size > 0):
             return False
-        #TO-DO
+        # TO-DO
         return True
 
 
@@ -117,18 +130,19 @@ while(option != 2):
             if(option == 1):
                 print("""
 				
-                    .-------.        .-''-.     .-'''-.   ___    _   .---. ,---------.  
-                    |  _ _   \     .'_ _   \   / _     \.'   |  | |  | ,_| \          \ 
-                    | ( ' )  |    / ( ` )   ' (`' )/`--'|   .'  | |,-./  )  `--.  ,---' 
-                    |(_ o _) /   . (_ o _)  |(_ o _).   .'  '_  | |\  '_ '`)   |   \    
-                    | (_,_).' __ |  (_,_)___| (_,_). '. '   ( \.-.| > (_)  )   :_ _:    
-                    |  |\ \  |  |'  \   .---..---.  \  :' (`. _` /|(  .  .-'   (_I_)    
-                    |  | \ `'   / \  `-'    /\    `-'  || (_ (_) _) `-'`-'|___(_(=)_)   
-                    |  |  \    /   \       /  \       /  \ /  . \ /  |        \(_I_)    
-                    ''-'   `'-'     `'-..-'    `-...-'    ``-'`-''   `--------`'---'    
-                                                                    
+                _______  .-./`)     .-''-.  ,---.   .--.,---.  ,---.   .-''-.  ,---.   .--..-./`)  ______         ,-----.     
+                \  ____  \\ .-.')  .'_ _   \ |    \  |  ||   /  |   | .'_ _   \ |    \  |  |\ .-.')|    _ `''.   .'  .-,  '.   
+                | |    \ |/ `-' \ / ( ` )   '|  ,  \ |  ||  |   |  .'/ ( ` )   '|  ,  \ |  |/ `-' \| _ | ) _  \ / ,-.|  \ _ \  
+                | |____/ / `-'`"`. (_ o _)  ||  |\_ \|  ||  | _ |  |. (_ o _)  ||  |\_ \|  | `-'`"`|( ''_'  ) |;  \  '_ /  | : 
+                |   _ _ '. .---. |  (_,_)___||  _( )_\  ||  _( )_  ||  (_,_)___||  _( )_\  | .---. | . (_) `. ||  _`,/ \ _/  | 
+                |  ( ' )  \|   | '  \   .---.| (_ o _)  |\ (_ o._) /'  \   .---.| (_ o _)  | |   | |(_    ._) ': (  '\_/ \   ; 
+                | (_{;}_) ||   |  \  `-'    /|  (_,_)\  | \ (_,_) /  \  `-'    /|  (_,_)\  | |   | |  (_.\.' /  \ `"/  \  ) /  
+                |  (_,_)  /|   |   \       / |  |    |  |  \     /    \       / |  |    |  | |   | |       .'    '. \_/``".'   
+                /_______.' '---'    `'-..-'  '--'    '--'   `---`      `'-..-'  '--'    '--' '---' '-----'`        '-----'     
+                                                                                                                                                                               
 					Ingresar matriz relación de la siguiente forma:  
-					recuerde, colocar un "*" (asterisco) al final para lograr su ejecución
+					recuerde, colocar un "*" (asterisco) al final para lograr su ejecución. 
+                    #IMPORTANTE# --> No evalua propiedad por propiedad; Se evalua en un conjuunto
 					> 1 0 1
 					> 1 1 1
 					> 1 0 0
@@ -145,10 +159,10 @@ while(option != 2):
                 matrix = np.array(myFirstMatrix, dtype=int)
                 matrix = RelationMatrix(matrix)
 
-                #Llamado a las funciones declaradas 
-                #Se compara si la matrix que ingresa el usuario 
-                #Contiene alguna de las propiedades indicadas
-				
+                # Llamado a las funciones declaradas
+                # Se compara si la matrix que ingresa el usuario
+                # Contiene alguna de las propiedades indicadas
+
                 isReflexiveMatrix = "SI" if isReflexive(matrix) else "NO"
                 isSymmetricMatrix = "SI" if isSymmetric(matrix) else "NO"
                 isAntisymmetricMatrix = "Si" if isAntisymmetric(
